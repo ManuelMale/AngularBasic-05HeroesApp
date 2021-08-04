d2d70014e35220a9fe23cb6d6021885f77c0d47e
ManuelMale <mmalerases@gmail.com>
2021-08-04 08:24:47 +0200

    Inicio de la sección 14, video 10.

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,89 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [`
+  img {
+    width: 100%;
+    border-radius: 5px;
+  }
+  `]
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    },
+  ];
 
-  ngOnInit(): void {
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
   }
 
+
+
+  constructor( private heroesService:     HeroesService,
+               private ActivatedRoute: ActivatedRoute,
+               private router: Router) { }
+
+  ngOnInit(): void {
+
+    if( !this.router.url.includes('editar') ) {
+      return;
+    }
+
+    this.ActivatedRoute.params
+     .pipe(
+       switchMap( ({id}) => this.heroesService.getHeroePorId(id) )
+     )
+      .subscribe( heroe => this.heroe = heroe );
+
+  }
+
+
+  guardar() {
+    if(this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+
+    if(this.heroe.id) {
+      // Actualizar
+      this.heroesService.actualizarHeroe(this.heroe)
+       .subscribe(heroe => console.log('Actualizando', heroe))
+    } else {
+      // Crear
+      this.heroesService.agregarHeroe(this.heroe)
+      .subscribe(heroe => {
+        this.router.navigate(['/heroes/editar', heroe.id]);
+      })
+    }
+
+  }
+
+  borrarHeroe() {
+    this.heroesService.borrarHeroe(this.heroe.id!)
+     .subscribe( resp => {
+
+       this.router.navigate(['/heroes']);
+
+     });
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
@@ -27,6 +28,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule
   ]
